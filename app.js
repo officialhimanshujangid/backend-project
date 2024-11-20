b4e5e8ecbca369f359632fe6fc2c19b0c38d0261
himanshuvigorous <himanshu.vigorous@gmail.com>
2024-11-20 11:21:46 +0530

    saved new changes

--- app.js
+++ app.js
@@ -15,23 +15,12 @@ app.post('/', (req, res) => {
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-app.get('/api/v1/tours/:id', (req, res) => {
-console.log(req.params);
-const id = req.params.id * 1
-
-
-if(id > tours.length){
-  return res.status(404).json({
-    status:"fail",
-    message : 'Innvalid Id'
-  })
-}
-const tour = tours.find(el => el.id === id)
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: {
-      ...tour
+      tours,
     },
   });
 });
@@ -53,6 +42,14 @@ app.post('/api/v1/tours', (req, res) => {
     }
   );
 });
+app.patch('api/v1/tours/:id',(req,res)=>{
+  res.status(200).json({
+    status:"success",
+    data:{
+      tours
+    }
+  })
+})
 
 const port = 8000;
 app.listen(port, () => {
